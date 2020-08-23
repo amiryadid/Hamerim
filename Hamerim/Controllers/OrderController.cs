fc7a134af30cc1ff871504682a5ec6ce654ac3de
Amir Yadid <amiryadid1@gmail.com>
2020-08-23 20:19:45 +0300

    Finished OrderController (Might be required to add additional features in the future)

--- Hamerim/Controllers/OrderController.cs
+++ Hamerim/Controllers/OrderController.cs
@@ -4,9 +4,11 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Hamerim.Data;
 using Hamerim.Models;
 using Hamerim.Services;
+using Microsoft.Ajax.Utilities;
 
 namespace Hamerim.Controllers
 {
@@ -40,18 +42,36 @@ namespace Hamerim.Controllers
                                                                 .Select(order => order.Date).ToList();
                 ViewBag.ServiceCategories = 
                     ctx.ServiceCategories.Include(category => category.ServicesInCategory).ToList();
-
             }
 
             return View();
         }
 
-        public ActionResult GetPopularServices(DateTime date)
+        public ActionResult GetPopularServices(int month)
         {
             List<Service> services = 
-                this.statisticsService.GetMostPopularServices(date.Month).ToList();
+                this.statisticsService.GetMostPopularServices(month).ToList();
 
             return Json(services);
+        }
+
+        [HttpPost]
+        public ActionResult BookOrder(Order order)
+        {
+            using (HamerimDbContext ctx = new HamerimDbContext())
+            {
+                ctx.Orders.Add(order);
+                ctx.SaveChanges();
+            }
+
+            return RedirectToAction("FinishedOrder", order.Id);
+        }
+
+        public ActionResult FinishedOrder(int orderNumber)
+        {
+            ViewBag.OrderNumber = orderNumber;
+
+            return View();
         }
     }
 }
