bd1a0dc0b6dde151a2982251bc5d4510395e8e53
Amir Yadid <amiryadid1@gmail.com>
2020-08-25 16:43:59 +0300

    Filter Controller method added

--- Hamerim/Controllers/OrderController.cs
+++ Hamerim/Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.WebPages;
 using Hamerim.Data;
 using Hamerim.Models;
 using Hamerim.Services;
@@ -27,9 +28,28 @@ namespace Hamerim.Controllers
             using (var ctx = new HamerimDbContext())
             {
                 ViewBag.Clubs = ctx.Clubs.Include(cl => cl.Address).ToList();
+                ViewBag.Cities = ctx.ClubAddresses.Select(address => address.City).Distinct().ToList();
             }
 
             return View();
+        }
+
+        public ActionResult FilterClubs(int maxPriceFilter=Int32.MaxValue, string nameFilter = "", string cityFilter = "")
+        {
+            using (var ctx = new HamerimDbContext())
+            {
+                IEnumerable<Club> filteredClubs = ViewBag.Clubs;
+
+                if (!nameFilter.IsEmpty())
+                    filteredClubs = filteredClubs.Where(club => club.Name.Contains(nameFilter));
+
+                if (!cityFilter.IsEmpty())
+                    filteredClubs = filteredClubs.Where(club => club.Address.City == cityFilter);
+
+                ViewBag.Clubs = filteredClubs.Where(club => club.Cost <= maxPriceFilter).ToList();
+            }
+
+            return Json(ViewBag.Clubs);
         }
 
         public ActionResult AfterChooseClub(int Id)
